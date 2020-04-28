2d005bd0fc05e6596e79a6911617cd85a5d25d1a
huntum33 <Huntum33+>
2020-04-29 00:39:39 +0200

    Initial commit

---
+++ src/main/java/io/sytac/springframework/spring5webapptraining/Spring5webapptrainingApplication.java
@@ -0,0 +1,13 @@
+package io.sytac.springframework.spring5webapptraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webapptrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5webapptrainingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/sytac/springframework/spring5webapptraining/Spring5webapptrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sytac.springframework.spring5webapptraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webapptrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
